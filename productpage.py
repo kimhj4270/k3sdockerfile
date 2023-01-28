e98a78b81f5d475063a7055ff87a0f9d633ed7de
kimhj4270 <a@a.com>
2023-01-28 15:07:55 +0000

    v2

--- productpage.py
+++ productpage.py
@@ -359,6 +359,7 @@ def getProducts():
     return [
         {
             'id': 0,
+#            'title': 'This is K3S Book info App!!',
             'title': 'This is update K3S Book info App!!',
             'descriptionHtml': '<a href="https://en.wikipedia.org/wiki/The_Comedy_of_Errors">Wikipedia Summary</a>: The Comedy of Errors is one of <b>William Shakespeare\'s</b> early plays. It is his shortest and one of his most farcical comedies, with a major part of the humour coming from slapstick and mistaken identity, in addition to puns and word play.'
         }
